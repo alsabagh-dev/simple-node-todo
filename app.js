eef2cf1f4bb762cce0183818cc954d8cf0c9727a
Abdelaziz Alsabagh <dev.alsabagh@gmail.com>
2022-03-09 23:04:00 +0200

    fix render statics

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // static files
-app.use('/assests', express.static('./public'));
+app.use(express.static('./public'));
 
 // fire controllers
 
